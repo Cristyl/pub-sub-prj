0cedc24fabbf6bc8e9529fc5ec79aac035fdc384
amarcvs <66919210+amarcvs@users.noreply.github.com>
2024-08-09 21:19:57 +0200

    Improvement and addition of some basic features

--- publisher.py
+++ publisher.py
@@ -1,18 +1,35 @@
-#!/usr/bin/env python
 import pika
 import sys
+import time
+import signal
 
 class Publisher():
     def __init__(self):
+        signal.signal(signal.SIGTERM, self.sigterm_handler)
+
         connection = pika.BlockingConnection(
             pika.ConnectionParameters(host='localhost'))
         channel = connection.channel()
 
-        channel.exchange_declare(exchange='logs1', exchange_type='fanout')
+        channel.exchange_declare(exchange=sys.argv[1], exchange_type='fanout')
 
-        message = ' '.join(sys.argv[1:]) or "info: Hello World!"
-        channel.basic_publish(exchange='logs1', routing_key='', body=message)
-        print(f" [x] Sent {message}")
-        connection.close()
+        # Question: Is it better the random crash in here or in the main?
+        print(f'[pub #{sys.argv[2]}] Connected to {sys.argv[1]} exchange', flush=True)
+
+        message = f"'Hello World! #{sys.argv[2]}'"
         
+        messages_to_send = 20
+        while messages_to_send:
+            channel.basic_publish(exchange=sys.argv[1], routing_key='', body=message)        
+            print(f"[pub #{sys.argv[2]}] Sent {message}", flush=True)
+            messages_to_send -= 1
+            time.sleep(1)
+
+        connection.close()
+
+    def sigterm_handler(self, signum, frame):
+        print(f"[pub #{sys.argv[2]}] Crashed", flush=True)
+        sys.exit(0)
+    
+
 Publisher()
